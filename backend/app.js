f1546b5022b4397c5e33696f3a9955b1f358935c
Brasseur Thibault <t.brasseur@activigie.com>
2021-07-11 13:43:18 +0200

    Second commit : Complete CRUD (architecture Routes,Models, Controllers) =>using express + mongoDB

---
+++ backend/app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+const app = express();
+
+mongoose.connect("mongodb+srv://Thibault:alicegenty84Z@clustersweethome.0pq9k.mongodb.net/ClusterSweetHome?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+
+
+module.exports = app;
+
+
+
+// =========> DEMO Midleware POUR TEST EN DUR
+// app.use((req, res, next) => {
+//     console.log('requete log dans console nodemon! ');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     res.status(201);
+//     next();
+// }); 
+
+// app.use((req, res, next) => {
+//     res.json({ message: "Votre réquéte express est reçue "});
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('réponse envoyer avec succés! ');    
+// });
+
+// =========> GET EN DUR POUR TEST
+// app.use('/api/stuff', (req, res, next) => {
+//     const stuff = [
+//       {
+//         _id: 'oeihfzeoi',
+//         title: 'Mon premier objet',
+//         description: 'Les infos de mon premier objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 4900,
+//         userId: 'qsomihvqios',
+//       },
+//       {
+//         _id: 'oeihfzeomoihi',
+//         title: 'Mon deuxième objet',
+//         description: 'Les infos de mon deuxième objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 2900,
+//         userId: 'qsomihvqios',
+//       },
+//     ];
+//     res.status(200).json(stuff);
+//   });
